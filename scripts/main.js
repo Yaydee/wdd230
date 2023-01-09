8c89d75071f10a6916c389986ae3ba993a97035a
Kyle Kerr <kkerr2550@gmail.com>
2023-01-09 11:33:34 -0700

    made some optimizations

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,8 @@
 let date = new Date();
 let year = date.getFullYear();
 
-console.log(year);
-console.log(date);
-
 document.querySelector('.year').innerHTML = year;
 
 let currentdate = document.lastModified;
-
-console.log(currentdate);
 
 document.querySelector('.updated').textContent = currentdate;
